9c1f087ba60b78d55a8bcda494e0d5c0a7f31794
Manish Patel <61538824+mk-patel@users.noreply.github.com>
2020-03-07 10:08:36 +0530

    new files uploaded

---
+++ applications.php
@@ -0,0 +1,208 @@
+<html>
+<head>
+  <title>Manish Patel</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="I am manish patel , this website will provide contents from manish patel.">
+    <meta name="keywords" content="manish patel, manish kumar,mkp,mpatel">
+    <meta name="author" content="Manish Patel">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="icon" type="image/png" href="11.png"/>
+<style>
+body{
+background:white;
+font-family:sans-serif;
+}
+header{
+width:100%;
+height:40px;
+background:grey;
+}
+.header-img{
+float:left;
+margin-left:10px;
+width:120px;
+height:40px;
+line-height:40px;
+font-size:15px;
+font-weight:700;
+color:white;
+}
+.header-img img{
+width:40px;
+height:40px;
+border:1px solid white;
+border-radius:50%;
+}
+.header-content{
+float:right;
+margin-right:10px;
+width:110px;
+height:40px;
+}
+.header-content img{
+width:40px;
+height:40px;
+border:1px solid white;
+border-radius:50%;
+}
+#category{
+margin-top:5px;
+}
+.dropdown-item{
+color:darkorange;
+font-size:13px;
+font-weight:700;
+}
+.row{
+margin-top:10px;
+}
+.bottom{
+width:100%;
+height:auto;
+background:grey;
+}
+.btm-elm{
+font-size:15px;
+padding:5px;
+margin-left:10px;
+}
+.btm-elm a{
+text-style:none;
+color:white;
+font-weight:700;
+}
+.last-line{
+font-size:11px;
+color:white;
+padding:5px;
+margin-left:10px;
+}
+.btn{
+font-size:12px;
+font-weight:700;
+padding:5px;
+}
+.pr{
+	font-size:27px;
+	color:darkorange;
+	padding:20px;
+	text-align:center;
+	margin-top:30px;
+	font-weight:700;
+}
+.explore-img{
+	text-align:center;
+}
+.explore-img img{
+width:130px;
+height:130px;
+}
+.pr-p{
+	color:black;
+	margin-left:20px;
+}
+.into-img img{
+width:220px;
+height:120px;
+margin-left:35px;
+}
+.caption{
+	font-size:14px;
+	color:black;
+	text-align:center;
+	padding:3px;
+}
+</style>
+</head>
+<body>
+<header>
+<div class="header-img">
+<img src="images/mp.jpg"> &nbsp;MKPATEL
+</div>
+<div class="header-content">
+<button id="category" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
+Categories
+</button>
+<ul class="dropdown-menu">         
+
+   <li><a class="dropdown-item" href="projects.php">My Projects</a></li>
+   <li class="divider"></li>
+   <li><a class="dropdown-item" href="applications.php">My Application</a></li>
+   <li class="divider"></li>
+   <li><a class="dropdown-item" href="tips.php">Tips</a></li>
+   <li class="divider"></li>
+   <li><a class="dropdown-item" href="entertainment.php">Entertainment</a></li>
+   <li class="divider"></li>
+   <li><a class="dropdown-item" href="index.html">Home</a></li>
+
+ 
+</ul>
+</div>
+</header>
+<div class="container">
+    <div class="row">
+         <img class="img-fluid" src="images/appp.jpg">
+		</div>
+	
+	<div class="row">
+		    <p class="pr-p">Some applications and programs are here 
+			which shows my knowledge and skill.<br>
+			These apps and programs are made by  me only.<br>
+			I hope you will like my hardwork. Thank You!
+			</p>
+		</div>
+		<div class="row">
+  <div class="col-md-2">
+    <div class="thumbnail">
+      <a href="morgaon/home.php">
+        <img  class="img-thumbnail" src="images/mor.jpg" alt="Mor Gaon - My Evillage" style="width:100%">
+        <div class="caption">
+          <p>Mor Gaon</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-2">
+    <div class="thumbnail">
+      <a href="#">
+        <img class="img-thumbnail" src="images/mkp.jpg" alt="MKPATEL - The Intro" style="width:100%">
+        <div class="caption">
+          <p>MKPATEL</p>
+        </div>
+      </a>
+    </div>
+  </div>
+    </div>
+    
+	<hr>
+    <div class="row">
+	    <h3  style="font-weight:700;color:purple;margin-left:10px;padding:5px;">
+		Mor Gaon - My Evillage
+		</h3>
+		<p style="color:grey;margin-left:40px;">It Connect all the villages with each other through technology.</span><br><br>
+        It is a website which provides the facility to share the activities of village like events, what happpening in village  etc.
+		It providse realtime news of the villages.
+		it is also provide the users 'profile management facility' like create, update and upload their information on site.
+</p>
+</div>
+<div class="row">
+		<div class="into-img">
+        <img src="images/morgaonproject.jpeg">
+		</div>
+    </div>
+	<hr>
+	<div class="row">
+	    <p class="pr-p">Multiple projects and tasks will be taken further...ThankYou!
+    </div>
+</div>
+<div class="bottom">
+<div class="btm-elm">
+<a href="feedback.php">Suggessions & Feedback</a> | <a href="about.html">About</a> | <a href="privacypolicy.html">Privacy Policy</a>
+</div>
+</div>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ entertainment.php
@@ -0,0 +1,264 @@
+<html>
+<head>
+  <title>Manish Patel</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="I am manish patel , this website will provide contents from manish patel.">
+    <meta name="keywords" content="manish patel, manish kumar,mkp,mpatel">
+    <meta name="author" content="Manish Patel">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="icon" type="image/png" href="11.png"/>
+<style>
+body{
+background:white;
+font-family:sans-serif;
+}
+header{
+width:100%;
+height:40px;
+background:grey;
+}
+.header-img{
+float:left;
+margin-left:10px;
+width:120px;
+height:40px;
+line-height:40px;
+font-size:15px;
+font-weight:700;
+color:white;
+}
+.header-img img{
+width:40px;
+height:40px;
+border:1px solid white;
+border-radius:50%;
+}
+.header-content{
+float:right;
+margin-right:10px;
+width:110px;
+height:40px;
+}
+.header-content img{
+width:40px;
+height:40px;
+border:1px solid white;
+border-radius:50%;
+}
+#category{
+margin-top:5px;
+}
+.dropdown-item{
+color:darkorange;
+font-size:13px;
+font-weight:700;
+}
+.row{
+margin-top:10px;
+}
+.bottom{
+width:100%;
+height:auto;
+background:grey;
+}
+.btm-elm{
+font-size:15px;
+padding:5px;
+margin-left:10px;
+}
+.btm-elm a{
+text-style:none;
+color:white;
+font-weight:700;
+}
+.last-line{
+font-size:11px;
+color:white;
+padding:5px;
+margin-left:10px;
+}
+.btn{
+font-size:12px;
+font-weight:700;
+padding:5px;
+}
+.pr{
+	font-size:35px;
+	color:darkorange;
+	padding:20px;
+	text-align:center;
+	margin-top:30px;
+	font-weight:700;
+}
+.explore-img{
+	text-align:center;
+}
+.explore-img img{
+width:130px;
+height:130px;
+}
+.pr-p{
+	color:black;
+	margin-left:20px;
+}
+.into-img img{
+width:220px;
+height:120px;
+margin-left:35px;
+}
+
+.morgaon-heading{
+	color:black;
+	font-size:20px;
+}
+.morgaon-description{
+	color:grey;
+	font-size:14px;
+}
+.main_sec{
+	width:100%;
+	height:auto;
+	box-shadow:1px 1px 10px 1px rgba(0,0,0,0.3);
+}
+.img_sec{
+	float:left;
+	width:120px;
+	height:100px;
+}
+.img_sec img{
+	width:120px;
+	height:100px;
+}
+.post_sec{
+	margin-left:130px;
+	padding:10px;
+}
+/* Style all font awesome icons */
+.fa {
+  padding: 0px;
+  font-size: 20px;
+  width: 20px;
+  text-align: center;
+  text-decoration: none;
+  border-radius: 50%;
+}
+
+/* Add a hover effect if you want */
+.fa:hover {
+  opacity: 0.7;
+}
+
+/* Set a specific color for each brand */
+
+/* Whatsapp */
+.fa-whatsapp {
+  background: green;
+  color: white;
+}
+</style>
+</head>
+<body>
+<header>
+<div class="header-img">
+<img src="images/mp.jpg"> &nbsp;MKPATEL
+</div>
+<div class="header-content">
+<button id="category" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
+Categories
+</button>
+<ul class="dropdown-menu">         
+
+   <li><a class="dropdown-item" href="projects.php">My Projects</a></li>
+   <li class="divider"></li>
+   <li><a class="dropdown-item" href="applications.php">My Application</a></li>
+   <li class="divider"></li>
+   <li><a class="dropdown-item" href="tips.php">Tips</a></li>
+   <li class="divider"></li>
+   <li><a class="dropdown-item" href="entertainment.php">Entertainment</a></li>
+   <li class="divider"></li>
+   <li><a class="dropdown-item" href="feedback.php">Feedback</a></li>
+
+ 
+</ul>
+</div>
+</header>
+<div class="container">
+    <div class="row">
+         <img class="img-fluid" src="images/ent.jpg">
+    </div>
+	<div class="row">
+         <p style="font-size:15px;color:black;font-weight:700;padding:10px;">
+		 Here you can find the interesting tips and amazing fact...
+		 </p>
+    </div>
+	<hr>
+	<div class="row">
+		  <div class="main_sec">
+		  <div class="img_sec"> 
+		  <img class="img-thumbnail" src="images/tipsf.png">
+		  </div>
+		  <div class="post_sec">
+		  It providse realtime news of the villages.it is also provide the users.<br>
+		  views .66 &nbsp; <a href="#" class="fa fa-whatsapp"></a>
+		  
+		  </div>
+		  </div>
+		</div>
+		<div class="row">
+		    <div class="main_sec">
+		  <div class="img_sec"> 
+		  <img class="img-thumbnail" src="images/tipsf.png">
+		  </div>
+		  <div class="post_sec">
+		  It providse realtime news of the villages.it is also provide the users.<br>
+		  views . 99 &nbsp; <a href="#" class="fa fa-whatsapp"></a>
+		  </div>
+		  </div>
+		</div>
+    <div class="row">
+	    <div class="main_sec">
+		  <div class="img_sec"> 
+		  <img class="img-thumbnail" src="images/tipsf.png">
+		  </div>
+		  <div class="post_sec">
+		  It providse realtime news of the villages.it is also provide the users.<br>
+		  views . 99 &nbsp; <a href="#" class="fa fa-whatsapp"></a>
+		  </div>
+		  </div>
+</div>
+<div class="row">
+		<div class="main_sec">
+		  <div class="img_sec"> 
+		  <img class="img-thumbnail" src="images/tipsf.png">
+		  </div>
+		  <div class="post_sec">
+		  It providse realtime news of the villages.it is also provide the users.<br>
+		  views . 99 &nbsp; <a href="#" class="fa fa-whatsapp"></a>
+		  </div>
+		  </div>
+    </div>
+	<div class="row">
+	    <div class="main_sec">
+		  <div class="img_sec"> 
+		  <img class="img-thumbnail" src="images/tipsf.png">
+		  </div>
+		  <div class="post_sec">
+		  It providse realtime news of the villages.it is also provide the users.<br>
+		  views . 99 &nbsp; <a href="#" class="fa fa-whatsapp"></a>
+		  </div>
+		  </div>
+    </div>
+</div>
+<hr>
+<div class="bottom">
+<div class="btm-elm">
+<a href="feedback.php">Suggessions & Feedback</a> | <a href="about.html">About</a> | <a href="privacypolicy.html">Privacy Policy</a>
+</div>
+</div>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
