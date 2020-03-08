6fa930a3160656a2e9affba82045e62f7ac488fb
Manish Patel <61538824+mk-patel@users.noreply.github.com>
2020-03-08 14:36:37 +0530

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -102,24 +102,7 @@ Suggessions & Feedback
 <hr style="border:2px solid green">
   </div>
 
-	 <?php
-require 'db.php';
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	$title = $mysqli->real_escape_string($_POST['name']);
-	$content = $mysqli->real_escape_string($_POST['content']);
-    $mobile = $mysqli->real_escape_string($_POST['mobile']);
-	$dt2=date("Y-m-d H:i:s");
-		
-	$sql = "INSERT INTO users(name,post,mobile,date)"
-                    . "VALUES('$title','$content','$mobile','$dt2')";	
-	mysqli_query($conn,$sql);
-    echo "<br>";
-    echo "<center style='color:green;'>Thankyou For Your Feedback.</center>";	
-    echo "<br>";
-    echo "<br>";
-}
 
-?>
 
 <div class="d-flex justify-content-center">
 <div class="container">
@@ -150,6 +133,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	 <?php
+require 'db.php';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$title = $mysqli->real_escape_string($_POST['name']);
+	$content = $mysqli->real_escape_string($_POST['content']);
+    $mobile = $mysqli->real_escape_string($_POST['mobile']);
+	$dt2=date("Y-m-d H:i:s");
+		
+	$sql = "INSERT INTO users(name,post,mobile,date)"
+                    . "VALUES('$title','$content','$mobile','$dt2')";	
+	mysqli_query($conn,$sql);
+    echo "<br>";
+    echo "<center style='color:green;'>Thankyou For Your Feedback.</center>";	
+    echo "<br>";
+    echo "<br>";echo "<br>";
+}
 
+?>
 </body>
 </html>
