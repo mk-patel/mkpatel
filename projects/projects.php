29b77712f9c0357c34cb7f1dfe1efd5627ea8638
Manish Patel <61538824+mk-patel@users.noreply.github.com>
2021-07-13 12:35:07 +0530

    Update projects.php

--- projects/projects.php
+++ projects/projects.php
@@ -1,22 +1,3 @@
-<?php
-/*session_start();
-$password=$_SESSION["password"];
-if(empty($password)){
-       header("location: login.php");
-         exit();
-	    }
-	else{
-        if ($_SESSION["password"] == "222")
-	    {
-	    }
-		else{
-		header("location: login.php");
-        exit();
-	    }
-	}
-	*/
-?>
-
 <html>
 <head>
     <meta charset="utf-8">
@@ -68,6 +49,13 @@ if(empty($password)){
 		margin-left:25px;
 		padding: 10px;
 	}
+	.pr-img{
+		text-align:center;
+	}
+	.pr-img img{
+		width:200px;
+		height:auto;
+	}
 	.morgaon-heading{
 		color:black;
 		font-size:20px;
@@ -78,11 +66,12 @@ if(empty($password)){
 	}
 	.pro-img{
 		margin-bottom:40px;
+		margin-left:35px;
 	}
 	.pro-img img{
 		width:220px;
 		height:auto;
-		margin-left:35px;
+		
 	}
 	.pro-manage
 	{
@@ -115,6 +104,50 @@ if(empty($password)){
 		 margin-left:40px;
 		 margin-right:40px;
 	}
+	@media screen and (max-width:700px){
+		.pr{
+			margin-top:20px;
+			color:#050c3b;
+			width:100%;
+			padding:10px;
+			text-align:center;
+			margin-left:0px;
+		}
+		.pr-p{
+			color:#050c3b;
+			font-size:15px;
+			padding: 10px;
+			text-align:center;
+			margin-left:0px;
+		}
+		.pro-manage h4{
+			font-weight:700;
+			margin-left:10px;
+			padding:30px;
+			color:#19566b;
+			font-size:18px;
+		}
+		.pro-manage h5{
+			color:black;
+			font-weight:500;
+			margin-left:40px;
+			margin-right:30px;
+			font-size:16px;
+		}
+		.pro-manage p{
+			 color:grey;
+			 margin-left:40px;
+			 margin-right:50px;
+			 padding:7px;
+			 font-size:14px;
+		}
+		.pro-manage ul li{
+			 color:grey;
+			 margin-left:40px;
+			 margin-right:40px;
+			 font-size:14px;
+		}
+	}
 	</style>
 </head>
 <body>
@@ -133,13 +166,20 @@ if(empty($password)){
 		</header>
 		<div class="container">
 			<div class="row">
-				<h2 class="pr">Projects & Ideas</h2>
-				<p class="pr-p">I have done some project to 
-					use and demonstrate my knowledge and skills.<br>
-					I always want to make a problem solver project, 
-					which can help to solve and improve qualities of the products.<br>
-					I hope you will like the projects.
-				</p>
+				<div class="col-sm-6">
+					<h2 class="pr">Projects & Ideas</h2>
+					<p class="pr-p">I have done some project to 
+						use and demonstrate my knowledge and skills.<br>
+						I always want to make a problem solver project, 
+						which can help to solve and improve qualities of the products.<br>
+						I hope you will like the projects.
+					</p>
+				</div>
+				<div class="col-sm-6">
+					<div class="pr-img">
+						<img src="../sys_images/psp.png"/>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -148,10 +188,15 @@ if(empty($password)){
 			<div class="pro-manage">
 				<div class="row">
 					<h4>
-					Mor Gaon - My Evillage
+					Mor Gaon - My E-Village | Digital Village
 					</h4>
 					<p>
-						<h5>Connects all the villages to each other to interchange information, news and events of any village.</h5>
+						<h5>Vision - Connecting all the villages with each other through a simple and secure platform.</h5>
+						<p>
+							Working excellently to exchange information like news data, event data, village data, or any common incidental data within the villages, on a common villager's friendly platform.
+							<br/>
+							'Villager's friendly' term means the platform is easy to use and easy to understand for villager's. Having the features like post liking, commenting and post sharing, also has user profile control system.
+						</p>
 						<ul>
 							<li>Easy to use platform.</li>
 							<li>Easy to find Recent News and Event details of any village.</li>
@@ -164,7 +209,9 @@ if(empty($password)){
 							<li>Having all the features to share and handle the information you shared.</li>
 							<li>All the above will help the people to grow digitally.</li>
 						</ul>
-						<p>You can see the project source code at Github - <a href="https://github.com/mk-patel/morgaon">https://github.com/mk-patel/morgaon </a></p>
+						<p>You can see the project source code at Github - <a href="https://github.com/mk-patel/morgaon">https://github.com/mk-patel/morgaon </a><br/><br/>
+							<a href="project-container/morgaon/morgaon"><button class="btn btn-secondary">&nbsp; Live Demo &nbsp;</button></a> &nbsp;
+						</p>
 					</p>
 				</div>
 				<div class="row">
@@ -180,7 +227,10 @@ if(empty($password)){
 					On Mock - Learn & Apply
 					</h4>
 					<p>
-						<h5>This project is mainly based on Education Application which provides notes and also takes test of the student. Here the user/student can evaluate their exam skills.</h5>
+						<h5>Vision - Learn different concepts for different Entrance Exams and take Mock Test based on Entrance Exam Pattern with favourable questions</h5>
+						<p>
+							Having all the features to make a test, and also have Notes section to learn concepts.
+						</p>
 						<ul>
 							<li>Easy to use platform.</li>
 							<li>In this app you will able to take tests see your marks and re take tests as practice for entrance exam.</li>
@@ -191,7 +241,8 @@ if(empty($password)){
 							<li>We do not tract any users.</li>
 							<li>We just provide you the way to test your skills</li>
 						</ul>
-						<p>You can see the project source code at Github - <a href="https://github.com/mk-patel/onmock">https://github.com/mk-patel/onmock </a></p>
+						<p>You can see the project source code at Github - <a href="https://github.com/mk-patel/onmock">https://github.com/mk-patel/onmock </a><br/><br/>
+						<a href="https://play.google.com/store/apps/details?id=com.testqom"><button class="btn btn-secondary">App Available - On Mock App </button></a>
 					</p>
 				</div>
 				<div class="row">
@@ -220,7 +271,8 @@ if(empty($password)){
 							<li>Having all the features to share and handle the information you shared.</li>
 							<li>All the above will help the people to grow digitally.</li>
 						</ul>
-						<p>You can see the project source code at Github - <a href="https://github.com/mk-patel/clsales">https://github.com/mk-patel/clsales </a></p>
+						<p>You can see the project source code at Github - <a href="https://github.com/mk-patel/clsales">https://github.com/mk-patel/clsales </a><br/><br/>
+							<a href="http://www.clsales.in"><button class="btn btn-secondary">Website Available - CL Sales </button></a>
 					</p>
 				</div>
 				<div class="row">
